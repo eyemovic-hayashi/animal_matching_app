e4881d48f26aa20786ac80530e7d07a9cb1e2170
mokomoka0733 <maocq10016@gmail.com>
2020-12-14 23:26:29 +0900

    userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         with_options presence:true do
+          validates :nickname
+          validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "名字を入力してください" }
+          validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "名前を入力してください" }
+          validates :laset_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "名字（カナ）を入力してください" }
+          validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "名前（カナ）を入力してください" }
+          validates :prefecture_id
+          validates :city
+         end
 end
